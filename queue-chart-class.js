d7e56d9c52ea745e2e0698b5e52bb751dba65556
ptrdo <ptrdo@users.noreply.github.com>
2019-08-15 12:54:51 -0700

    Resources

---
+++ queue-chart-class.js
@@ -0,0 +1,55 @@
+/**
+ * queue-chart
+ * Dashboard visualiser of cluster traffic
+ *
+ * @author psylwester(at)idmod(dot)org
+ * @version 1.00, 2019/06/12
+ * @requires (framework)
+ *
+ */
+
+const CONFIG = {};
+
+const collection = {
+
+  input: {},
+  output: {},
+
+  transform: function (input) {
+
+    input["transformed"] = true;
+
+    this.output = input;
+  },
+
+  get latest() {
+    return this.output;
+  },
+
+  set newest(data) {
+    this.input = data;
+    this.transform(data);
+  }
+
+};
+
+const render = function () {
+
+  return this;
+};
+
+const update = function (data) {
+
+  collection.newest = data;
+};
+
+const query = function () {
+
+  return collection.latest;
+};
+
+const destroy = function () {
+
+};
+
+export default { render, update, query, destroy }
